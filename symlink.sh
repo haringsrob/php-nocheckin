3a9e2ad883c0f56b91d6f161100d57de781b8d0f
Harings Rob <haringsrob@gmail.com>
2021-04-15 07:43:32 +0200

    Fix symlink.

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # @NOCHECKIN
-if [ ! -L .git/hooks/nocheckin-pre-commit-hook ];
+if [ ! -f .git/hooks/nocheckin-pre-commit-hook ];
 then
     echo "symlinking .. nocheckin hook"
-    ln -s ../nocheckin-pre-commit-hook .git/hooks/nocheckin-pre-commit-hook
+    ln ./nocheckin-pre-commit-hook .git/hooks/nocheckin-pre-commit-hook
 else
     echo ".nocheckin is already a symlink"
 fi
